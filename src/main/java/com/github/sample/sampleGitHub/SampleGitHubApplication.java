152c843291e8fd2f3d4f1b6d9431732202d99ec0
Lenovo <Lenovo@DESKTOP-4R0NM28>
2023-03-25 12:47:49 +0530

    First Commit

---
+++ src/main/java/com/github/sample/sampleGitHub/SampleGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.github.sample.sampleGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sample/sampleGitHub/SampleGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sample.sampleGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
